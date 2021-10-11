4e2f6c7604d69df6be884832803a8f20d9c3731a
Julian Currie <julian.currie@gmail.com>
2021-10-11 17:21:48 -0400

    removed const keywords

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('The Flutter Factory'),
+        title: Text('The Flutter Factory'),
       ),
-      body: const Center(
+      body: Center(
         child: Text('Hello World'),
       ),
     );
