a58a6025c57957a959b3f4977f659db7b039236f
Julian Currie <julian.currie@gmail.com>
2021-10-17 21:49:39 -0400

    Created expanded app bar, tabs and icons

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,14 +3,84 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
 
+  TabBarView _buildTabBarView() {
+    return TabBarView(
+      children: [
+        Container(color: Colors.deepPurple),
+        Container(color: Colors.red),
+      ],
+    );
+  }
+
+  TabBar _buildTabBar() {
+    return TabBar(
+      indicatorWeight: 5,
+      indicatorColor: Colors.white,
+      labelColor: Colors.blue,
+      unselectedLabelColor: Colors.grey,
+      tabs: [
+        Tab(
+          icon: Icon(Icons.video_collection),
+          child: Text('Videos'),
+        ),
+        Tab(
+          icon: Icon(Icons.library_books),
+          child: Text('Docs'),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('The Flutter Factory'),
-      ),
-      body: Center(
-        child: Text('Hello World'),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (_, __) {
+            return <Widget>[
+              SliverAppBar(
+                title: Text('Home'),
+                backgroundColor: Colors.black,
+                pinned: true,
+                floating: true,
+                snap: true,
+                leading: Image.asset('images/logo.png'),
+                expandedHeight: 300,
+                bottom: _buildTabBar(),
+                actions: [
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: IconButton(
+                      icon: Icon(Icons.info),
+                      color: Colors.blue,
+                      onPressed: () => print('Map pressed'),
+                      highlightColor: Colors.blue,
+                      splashColor: Colors.purple,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: IconButton(
+                      color: Colors.blue,
+                      icon: Icon(Icons.settings),
+                      onPressed: () => print('Settings pressed'),
+                      highlightColor: Colors.blue,
+                      splashColor: Colors.purple,
+                    ),
+                  ),
+                ],
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Padding(
+                    padding: EdgeInsets.symmetric(vertical: kToolbarHeight),
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: _buildTabBarView(),
+        ),
       ),
     );
   }
