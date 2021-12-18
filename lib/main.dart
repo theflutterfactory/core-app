ed36b7d0ccb1e85b5068cfeb2a5985b3140f5e35
Julian Currie <julian.currie@gmail.com>
2021-12-18 12:48:07 -0500

    Added code for part 3 - Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutterfactory/controller/video_notifier.dart';
 import 'package:flutterfactory/view/home/home.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(
@@ -13,30 +13,35 @@ void main() {
   );
 }
 
-class FlutterFactory extends StatefulWidget {
-  FlutterFactory({Key? key}) : super(key: key);
-
-  @override
-  State<FlutterFactory> createState() => _FlutterFactoryState();
-}
-
-class _FlutterFactoryState extends State<FlutterFactory> {
-  @override
-  void initState() {
-    super.initState();
-    SchedulerBinding.instance?.addPostFrameCallback((timeStamp) {
-      Provider.of<VideoNotifier>(context, listen: false).fetchVideos();
-    });
-  }
+class FlutterFactory extends StatelessWidget {
+  const FlutterFactory({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        print(snapshot);
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: Home(),
+          );
+        }
+
+        return Center(
+          child: SizedBox(
+            height: 100,
+            width: 100,
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.white,
+            ),
+          ),
+        );
+      },
     );
   }
 }
